d9984d17421ccb24573cc8b7c61503208d4564fc
Ali-M24 <ali24mohammadzadeh@gmail.com>
2023-03-12 23:42:22 +0330

    save and finish this project and push to repository

--- lib/main.dart
+++ lib/main.dart
@@ -37,30 +37,44 @@ class Application extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           SizedBox(width: double.infinity),
-          _getheader(),
+          _getMainContent(),
         ],
       ),
     );
   }
 
-  Widget _getheader() {
+  Widget _getMainContent() {
     return Column(
       children: [
         SizedBox(height: 20.0),
+        _getMainContentHeader(),
+        SizedBox(height: 12.0),
+        _getRowIcons(),
+        SizedBox(height: 12),
+        _getSkillCards(),
+        SizedBox(height: 12.0),
+        _getResume()
+      ],
+    );
+  }
+
+  Widget _getMainContentHeader() {
+    return Column(
+      children: [
         CircleAvatar(
           backgroundImage: AssetImage('assets/images/user.png'),
           radius: 80.0,
         ),
         SizedBox(height: 20.0),
-        Text('علی محمدزاده هستم برنامه نویس فول استک',
-            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w900)),
+        Text(
+          'علی محمدزاده هستم برنامه نویس فول استک',
+          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w900),
+        ),
         SizedBox(height: 20.0),
         Text(
           'برنامه نویس موبایل و وب هستم و دوس دارم هرچیزی رو که یاد میگیرم یاد بدم',
           textAlign: TextAlign.center,
         ),
-        SizedBox(height: 12.0),
-        _getRowIcons(),
       ],
     );
   }
@@ -90,6 +104,70 @@ class Application extends StatelessWidget {
           color: Colors.blueGrey,
         ),
       ],
+    );
+  }
+
+  Widget _getSkillCards() {
+    var titles = ['Flutter', 'Android', 'Java', 'Dart', 'Kotlin'];
+    return Wrap(
+      children: [
+        for (var path in titles)
+          Padding(
+            padding: EdgeInsets.all(4.0),
+            child: Card(
+              elevation: 6,
+              child: Column(
+                children: [
+                  Container(
+                    height: 64.0,
+                    child: Image(
+                      image: AssetImage('assets/images/$path.png'),
+                      width: 60.0,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('$path'),
+                  )
+                ],
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+
+  Widget _getResume() {
+    var myResumeTexts = [
+      'برنامه نویس فرانت اند در شرکت پلاتین',
+      'برنامه نویس فریلنسر موبایل',
+      'برنامه نویس بک اند با نود جی اس و اکسپرس'
+    ];
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      width: double.infinity,
+      color: Colors.grey[200],
+      child: Column(
+        children: [
+          Text(
+            'سابقه کاری من',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+          ),
+          SizedBox(height: 12.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              SizedBox(width: double.infinity),
+              for (var resumeText in myResumeTexts)
+                Text(
+                  '$resumeText',
+                  textDirection: TextDirection.rtl,
+                )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
